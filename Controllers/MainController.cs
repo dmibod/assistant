4dd33a61950e702679544badaae12852d4e09e24
dmibod <bodnardm@gmail.com>
2022-10-26 12:29:51 +0300

    Add CRUD ops

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,20 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace myapp.Controllers;
 
-namespace myapp.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
-[Route("myapp")]
+[Route("people")]
 public class MainController : ControllerBase
 {
-    [HttpGet("all")]
+    private static List<Person> persons = new()
+    {
+        new Person { Name = "Ana" },
+        new Person { Name = "Felipe" },
+        new Person { Name = "Emillia" }
+    };
+
+    [HttpGet]
     public ActionResult<IEnumerable<Person>> GetAll()
     {
-        return new []
+        return persons;
+    }
+
+    [HttpGet("{name}")]
+    public ActionResult<Person> One(string name)
+    {
+        var person = persons.FirstOrDefault(p => p.Name == name);
+
+        return person == null ? this.NotFound() : person;
+    }
+    
+    [HttpPatch("{name}")]
+    public ActionResult<Person> Rename(string name, string newName)
+    {
+        var person = persons.FirstOrDefault(p => p.Name == name);
+
+        if (person != null)
         {
-            new Person { Name = "Ana" },
-            new Person { Name = "Felipe" },
-            new Person { Name = "Emillia" }
-        };
+            person.Name = newName;
+            
+            return person;
+        }
+        
+        return this.NotFound();
+    }
+    
+    [HttpDelete("{name}")]
+    public ActionResult<Person> Remove(string name)
+    {
+        var person = persons.FirstOrDefault(p => p.Name == name);
+
+        if (person != null)
+        {
+            persons.Remove(person);
+            return person;
+        }
+
+        return this.NotFound();
     }
 }
 
