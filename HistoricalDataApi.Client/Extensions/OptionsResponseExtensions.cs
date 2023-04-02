dc343db326cf5aeb5c588ecdbb54183b1125e5af
dmibod <bodnardm@gmail.com>
2023-04-02 20:54:42 +0300

    Add extension methods

---
+++ HistoricalDataApi.Client/Extensions/OptionsResponseExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace HistoricalDataApi.Client.Extensions;
+
+public static class OptionsResponseExtensions
+{
+    public static IEnumerable<OptionContractData> GetContracts(this OptionExpirationData data)
+    {
+        if (data == null)
+        {
+            throw new ArgumentNullException(nameof(data));
+        }
+        
+        var empty = Array.Empty<OptionContractData>();
+        var calls = data.Options == null ? empty : data.Options.Calls ?? empty;
+        var puts = data.Options == null ? empty : data.Options.Puts ?? empty;
+
+        return calls.Union(puts);
+    }
+}
