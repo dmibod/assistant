65216a8a6449f35b41670adbc513c111839490ac
dmibod <bodnardm@gmail.com>
2023-04-25 09:44:44 +0300

    Add check token / create tenant endpoint

--- Tenant/Assistant.Tenant.Api/Controllers/TenantController.cs
+++ Tenant/Assistant.Tenant.Api/Controllers/TenantController.cs
@@ -1,6 +1,8 @@
 ﻿namespace Assistant.Tenant.Api.Controllers;
 
+using System.Security.Claims;
 using Assistant.Tenant.Core.Services;
+using Common.Core.Security;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
@@ -8,10 +10,38 @@ using Microsoft.AspNetCore.Mvc;
 public class TenantController : ControllerBase
 {
     private readonly IPositionService positionService;
+    private readonly ITenantService tenantService;
+    private readonly IIdentityProvider identityProvider;
 
-    public TenantController(IPositionService positionService)
+    public TenantController(IPositionService positionService, ITenantService tenantService, IIdentityProvider identityProvider)
     {
         this.positionService = positionService;
+        this.tenantService = tenantService;
+        this.identityProvider = identityProvider;
+    }
+
+    [HttpPost("Token")]
+    public async Task<ActionResult> Token()
+    {
+        var identity = this.identityProvider.Identity as ClaimsIdentity;
+        if (identity == null)
+        {
+            return this.BadRequest();
+        }
+
+        var tenant = await this.tenantService.GetOrCreateAsync();
+
+        var claim = identity.Claims.Single(c => c.Type == "exp").Value;
+        var value = long.Parse(claim);
+        var exp = DateTime.UnixEpoch.AddSeconds(value);
+
+        var result = new
+        {
+            User = tenant.Name,
+            Expiration = exp.ToLongDateString()
+        };
+
+        return this.Ok(result);
     }
 
     [HttpGet("Positions")]
